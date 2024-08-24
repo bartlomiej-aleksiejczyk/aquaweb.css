16a8f481ecc0ee1a83c4388e2a1e04265ce9da34
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-08-24 12:35:32 +0200

    Refactor build logic

--- src/bundleCss.js
+++ src/bundleCss.js
@@ -1,28 +1,47 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import { bundle } from "lightningcss";
 
+async function createDirectory(directory) {
+  try {
+    await fs.mkdir(directory, { recursive: true });
+    console.log(`Directory ${directory} created or already exists.`);
+  } catch (err) {
+    console.error(`Failed to create the directory: ${directory}`, err);
+    throw err;
+  }
+}
+
+async function writeFile(filePath, content) {
+  try {
+    await fs.writeFile(filePath, content);
+    console.log(`File ${filePath} successfully written!`);
+  } catch (err) {
+    console.error(`Error writing file: ${filePath}`, err);
+    throw err;
+  }
+}
+
+async function bundleAndSave(inputPath, outputPath, minify) {
+  console.log(`Processing: ${inputPath}`);
+  const outputDir = path.dirname(outputPath);
+
+  try {
+    let { code } = bundle({
+      filename: inputPath,
+      minify: minify,
+    });
+
+    await createDirectory(outputDir);
+
+    await writeFile(outputPath, code);
+  } catch (err) {
+    console.error("Failed to process CSS bundle:", err);
+  }
+}
+
 const inputPath = process.argv[2];
 const outputPath = process.argv[3];
-const outputDir = path.dirname(outputPath);
 const minify = process.argv[4] === "true";
 
-console.log(inputPath);
-let { code, map } = bundle({
-  filename: inputPath,
-  minify: minify,
-});
-
-fs.mkdir(outputDir, { recursive: true }, (err) => {
-  if (err) {
-    console.error("Failed to create the directory:", err);
-  } else {
-    fs.writeFile(outputPath, code, (err) => {
-      if (err) {
-        console.error("Error writing file after creating directory:", err);
-      } else {
-        console.log("File successfully written!");
-      }
-    });
-  }
-});
+bundleAndSave(inputPath, outputPath, minify);
